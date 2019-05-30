9396226a045293401b304ab5fa98b14e3786fb44
Xander Stevenson <xander@Xanders-MacBook-Pro.local>
2019-05-30 16:09:42 -0600

    yes, finally have the 'NowPlaying' section done

--- ultimate-movie-db/src/pages/NowPlaying/NowPlaying.js
+++ ultimate-movie-db/src/pages/NowPlaying/NowPlaying.js
@@ -13,7 +13,7 @@ function getTMDBdata(){
     axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=15e1bbc41548aa6ea02bac4f092efa6f&language=en-US&page=1')
     .then(response => JSON.stringify(response))
     .then(response => {
-        info = response
+        info = response.toString().split(',')
     })
     .catch(function(error){
         alert(error)
@@ -21,27 +21,45 @@ function getTMDBdata(){
     return info
 }
 
-function getTitle(str){
-    var list = ''
-    while(str.includes('title')){
-       let indexTitle = str.indexOf('title')
-       let indexPop = str.indexOf('popularity')
-        let word = str.slice(indexTitle + 7,indexPop - 2)
-        var newword = word.toString().replace(/\"/g, "")
-        str.replace(/\title/g, "")
-        return newword
-       }
 
+function getTitleNames(str){
+    var str2 = str.toString()
+    var count = 0
+    var list2 = str2.split('"')
+    var list3 = []
+    for(let i = 0; i < list2.length; i ++){
+        if(list2[i] == "title"){
+            count +=1
+            list2[i].replace('title', '')
+            list3.push(list2[i + 2] + ", ")
+        }
+    }
+    return list3
 }
 
-getTitle(getTMDBdata())
+
+function getNiceListofTitles(list){
+    var newlist = ''
+    // for(let i = 0; i < list.length; i++){
+    //  newlist += list[i] + "/n"
+    // }
+    return list.map((item)=>{
+      return <p>{item}</p>
+    })
+}
+
+
 
 function NowPlaying() {
 
     return ( 
         <div>
-        <h1 className='page-name'>Now Playing</h1>  
-        <span>{getTitle(getTMDBdata())}</span>
+            <br></br>
+            <h1 className='page-name'>Now Playing</h1>  
+            <br></br>
+                <span><h2>{`There following movies currently playing in theatres:`}</h2></span>
+                {/* <span>{getTitleNames(getTMDBdata())}</span> */}
+                <span>{getNiceListofTitles(getTitleNames(getTMDBdata()))}</span>
         </div>
     )
     }
