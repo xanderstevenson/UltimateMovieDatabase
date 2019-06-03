4515aa2842aa1094ed1b0760f2417a9449b0d04b
Xander Stevenson <xander@Xanders-MacBook-Pro.local>
2019-06-03 13:07:04 -0600

    Updates

---
+++ precache-manifest.71288bf70419367d9a03bbc532d4c0a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e4a2b910aa17be33b6e645d785ac5b",
+    "url": "/xanderstevenson/UltimateMovieDatabase/index.html"
+  },
+  {
+    "revision": "001f77c887133ed18a37",
+    "url": "/xanderstevenson/UltimateMovieDatabase/static/css/main.4e1a716c.chunk.css"
+  },
+  {
+    "revision": "cfd1e7506ec3b857cb61",
+    "url": "/xanderstevenson/UltimateMovieDatabase/static/js/2.c591d792.chunk.js"
+  },
+  {
+    "revision": "001f77c887133ed18a37",
+    "url": "/xanderstevenson/UltimateMovieDatabase/static/js/main.cc6b06d9.chunk.js"
+  },
+  {
+    "revision": "88d6e499744965a98e0f",
+    "url": "/xanderstevenson/UltimateMovieDatabase/static/js/runtime~main.7255291e.js"
+  }
+]);
