c0a5ab61f88f3fc8c1a0b4b69828dd424fbbb209
Xander Stevenson <xander@Xanders-MacBook-Pro.local>
2019-06-03 11:59:59 -0600

    all requirements met

--- ultimate-movie-db/src/pages/Search/Search.js
+++ ultimate-movie-db/src/pages/Search/Search.js
@@ -7,8 +7,8 @@ var info = []
 
 
 function getTMDBdata(vid){
-  let vid2 = vid
-  vid = vid2
+  let vid2 = vid + vid.charAt(vid.length - 1)
+
   // alert(vid2)
   axios.get('https://api.themoviedb.org/3/search/movie?query=' + vid + '&api_key=15e1bbc41548aa6ea02bac4f092efa6f&language=en-US&page=1&include_adult=false')
  
@@ -28,20 +28,30 @@ function getTitleNames(str){
   var count = 0
   var list2 = str2.split('"')
   var list3 = []
+  var movieID = 0
   for(let i = 0; i < list2.length; i ++){
+    if(list2[i] == "id"){
+      movieID = list2[i + 1].substring(1, list2[i + 1].length - 1)
+
+    }
       if(list2[i] == "title"){
           count +=1
           list2[i].replace('title', '')
-          list3.push(list2[i + 2] + ", ")
-      }
+          list3.push(list2[i + 2] + '$' +movieID + ", ")
+      }    
   }
   return list3
 }
 
 function getNiceListofTitles(list){
-  var newlist = ''
+  
+  
   return list.map((item)=>{
-    return <tr><td><a class="link"  target="#blank">{item}</a></td></tr>
+    let moneyIndex = item.indexOf('$')
+    let movieID = item.slice(moneyIndex + 1, item.length - 2)
+    let item2 = item.slice(0, moneyIndex)
+    var movieURL = 'https://www.themoviedb.org/movie/' + movieID + '?language=en-US'
+    return <tr><td><a class="link" href={movieURL} target="#blank">{item2}</a></td></tr>
 
   
 })
@@ -87,7 +97,7 @@ class Search extends Component {
         <br></br>
         <span>
             <form>
-            <label class="label" htmlFor="movieSearch">Search for a Movie: </label>
+            <label class="label" htmlFor="movieSearch">Type a Word or Phrase and Hit the SPACE BAR: </label>
             <br></br>
             <br></br>
             <input type="text"  value={this.state.data} name="movieSearch" onChange={this.handleChange} id="movieSearch" placeholder='moviename' />
