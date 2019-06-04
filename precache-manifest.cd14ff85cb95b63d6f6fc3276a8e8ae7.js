750a27a67fe9c97607cb5a59e81b7c866a6a11e2
Xander <xanderstevenson2@gmail.com>
2019-06-03 21:40:33 -0600

    Updates

--- precache-manifest.cd14ff85cb95b63d6f6fc3276a8e8ae7.js
+++ precache-manifest.cd14ff85cb95b63d6f6fc3276a8e8ae7.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c74ea5f8095bf642461532ccb26e1059",
+    "revision": "9cbf70e84d3888fc3b34afaad3df3896",
     "url": "/UltimateMovieDatabase/index.html"
   },
   {
-    "revision": "e9aee9031680b9d59ddc",
-    "url": "/UltimateMovieDatabase/static/css/main.2eacf449.chunk.css"
+    "revision": "2efc39b353c39688d789",
+    "url": "/UltimateMovieDatabase/static/css/main.c42cd500.chunk.css"
   },
   {
     "revision": "bf04bbe2acd88756ccaf",
     "url": "/UltimateMovieDatabase/static/js/2.fbe3a724.chunk.js"
   },
   {
-    "revision": "e9aee9031680b9d59ddc",
+    "revision": "2efc39b353c39688d789",
     "url": "/UltimateMovieDatabase/static/js/main.8601b7b3.chunk.js"
   },
   {
